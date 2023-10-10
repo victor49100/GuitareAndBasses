0b17880dd33d522aa6dc926c0807c2b27b4247e6
Major <moreauvictor1906@gmail.com>
2023-10-10 19:53:03 +0200

    ajout realisation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RealisationComponent } from './realisation/realisation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RealisationComponent
   ],
   imports: [
     BrowserModule,
