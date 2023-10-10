d79c1188824b03490ebe8116ebfbb1faf92a5f6a
Major <moreauvictor1906@gmail.com>
2023-10-10 22:50:01 +0200

    Contact

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RealisationComponent } from './realisation/realisation.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RealisationComponent
+    RealisationComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
