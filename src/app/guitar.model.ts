d9d24e4add1ce9fe28e4194c62188890bb048275
MAJOR <moreauvictor1906@gmail.com>
2023-12-02 18:02:21 +0100

    Ajout Model Guitares pour affichage du details

---
+++ src/app/guitar.model.ts
@@ -0,0 +1,8 @@
+// guitar.model.ts
+
+export interface Guitar {
+  id: string;
+  name: string;
+  description: string;
+  images: string[];
+}
---
+++ src/app/guitar.service.ts
@@ -0,0 +1,33 @@
+// guitar.service.ts
+
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Guitar } from './guitar.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GuitarService {
+  private guitars: Guitar[] = [
+    {
+      id: '1',
+      name: 'Guitar 1',
+      description: 'Description of Guitar 1',
+      images: ['assets/img/Guitare1/1.webp', 'assets/img/Guitare1_2.webp', 'assets/img/Guitare1_3.webp'],
+    },
+    {
+      id: '2',
+      name: 'Guitar 2',
+      description: 'Description of Guitar 2',
+      images: ['url4', 'url5', 'url6'],
+    },
+  ];
+
+  getGuitars(): Observable<Guitar[]> {
+    return of(this.guitars);
+  }
+
+  getGuitarById(id: string): Guitar | undefined {
+    return this.guitars.find((guitar) => guitar.id === id);
+  }
+}
